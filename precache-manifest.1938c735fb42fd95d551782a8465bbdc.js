4bc637874524f508704f467eced979ea6d090d09
Iurii Petrov <yurapetrov91@gmail.com>
2020-04-22 11:36:58 +0300

    Updates

---
+++ precache-manifest.1938c735fb42fd95d551782a8465bbdc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa5b35d80472859b4aeadbed24b31d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9de66c314b4ee8a59bd4",
+    "url": "./static/css/main.3287f9e5.chunk.css"
+  },
+  {
+    "revision": "b23cd99607aafc7c9c87",
+    "url": "./static/js/2.bc76db86.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.bc76db86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9de66c314b4ee8a59bd4",
+    "url": "./static/js/main.42f3c6dc.chunk.js"
+  },
+  {
+    "revision": "d34856cc17b978b4a115",
+    "url": "./static/js/runtime-main.9d7a78c6.js"
+  }
+]);
